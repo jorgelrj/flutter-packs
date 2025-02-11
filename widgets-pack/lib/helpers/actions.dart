2f948cd1a7146ed7c9e11360c9bd0a87f85c5e22
Jorge Luis Rodrigues Junior <jr.luisrodrigues@gmail.com>
2025-02-11 10:54:27 -0300

    feat: Enhance AppAction with styling and custom builder support
    - Add `style` property to AppAction for text styling
    - Introduce AppActionBuilder for custom action widget generation
    - Refactor AppActionExtension to support new action types and styling
    - Bump version to 0.6.12

--- widgets-pack/lib/helpers/actions.dart
+++ widgets-pack/lib/helpers/actions.dart
@@ -5,12 +5,14 @@ class AppAction<M extends Object> {
   final Widget _icon;
   final void Function()? onPressed;
   final String? tooltip;
+  final TextStyle? style;
 
   const AppAction({
     required this.label,
     Widget? icon,
     this.onPressed,
     this.tooltip,
+    this.style,
   }) : _icon = icon ?? const SizedBox();
 
   Widget get icon => _icon;
@@ -31,31 +33,48 @@ class AppActionsGroup<M extends Object> extends AppAction<M> {
     required super.label,
     required this.items,
     super.icon,
+    super.style,
   });
+}
+
+class AppActionBuilder<M extends Object> extends AppAction<M> {
+  final Widget Function() builder;
+
+  const AppActionBuilder({
+    required this.builder,
+  }) : super(
+          label: '',
+          icon: const SizedBox(),
+        );
+}
+
+extension AppActionExtension<M extends Object> on AppAction<M> {
+  Widget toAnchorChild() {
+    return switch (this) {
+      AppActionDivider() => const Divider(),
+      final AppActionBuilder builder => builder.builder(),
+      final AppActionsGroup group => SubmenuButton(
+          menuChildren: group.items.toAnchorChildren(),
+          leadingIcon: group.icon,
+          child: Text(group.label, style: group.style),
+        ),
+      final AppAction action => TooltipVisibility(
+          visible: action.tooltip != null,
+          child: Tooltip(
+            message: action.tooltip ?? action.label,
+            child: MenuItemButton(
+              onPressed: action.onPressed,
+              leadingIcon: action.icon,
+              child: Text(action.label, style: action.style),
+            ),
+          ),
+        ),
+    };
+  }
 }
 
 extension AppActionListExtension<M extends Object> on List<AppAction<M>> {
   List<Widget> toAnchorChildren() {
-    return map((action) {
-      return switch (action) {
-        AppActionDivider() => const Divider(),
-        final AppActionsGroup group => SubmenuButton(
-            menuChildren: group.items.toAnchorChildren(),
-            leadingIcon: group.icon,
-            child: Text(group.label),
-          ),
-        final AppAction action => TooltipVisibility(
-            visible: action.tooltip != null,
-            child: Tooltip(
-              message: action.tooltip ?? action.label,
-              child: MenuItemButton(
-                onPressed: action.onPressed,
-                leadingIcon: action.icon,
-                child: Text(action.label),
-              ),
-            ),
-          ),
-      };
-    }).toList();
+    return map((action) => action.toAnchorChild()).toList();
   }
 }
