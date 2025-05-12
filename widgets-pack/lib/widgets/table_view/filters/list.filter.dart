9d110e22cce772fed007a9bb3407c412ab027c76
Jorge Luis Rodrigues Junior <jr.luisrodrigues@gmail.com>
2025-05-12 09:44:59 -0300

    feat: Bump version to 0.7.4 and enhance AppListFilter with improved functionality
    - Bump package version to 0.7.4
    - Refactor AppListFilter to extend AppFilter for better structure
    - Introduce asString parameter for customizable item display
    - Implement state management improvements in _AppListFilterState
    - Enhance build method with overlay functionality for item selection

--- widgets-pack/lib/widgets/table_view/filters/list.filter.dart
+++ widgets-pack/lib/widgets/table_view/filters/list.filter.dart
@@ -1,26 +1,168 @@
 part of 'filter.dart';
 
-class AppListFilter<T extends Object> extends StatefulWidget {
+class AppListFilter<T extends Object> extends AppFilter<T> {
   final List<T> items;
   final String label;
   final ValueChanged<T?> onChanged;
   final T? initialValue;
+  final String Function(T)? asString;
 
   const AppListFilter({
     required this.label,
     required this.onChanged,
     required this.items,
     this.initialValue,
+    this.asString,
     super.key,
   });
 
   @override
-  State<AppListFilter> createState() => _AppListFilterState();
+  State<AppFilter<T>> createState() => _AppListFilterState<T>();
 }
 
-class _AppListFilterState extends State<AppListFilter> {
+class _AppListFilterState<T extends Object> extends _AppFilterState<T> {
+  @override
+  AppListFilter<T> get widget => super.widget as AppListFilter<T>;
+
+  late final _itemNotifier = ValueNotifier<T?>(
+    widget.initialValue,
+  );
+
+  late final _textController = TextEditingController(
+    text: _formattedItem,
+  );
+
+  late final _overlayController = AppOverlayPortalController(
+    onHide: _setValue,
+  );
+
+  final _link = LayerLink();
+
+  String get _formattedItem {
+    if (_itemNotifier.value == null) {
+      return '';
+    }
+
+    if (widget.asString != null) {
+      return widget.asString!(_itemNotifier.value!);
+    }
+
+    return _itemNotifier.value.toString();
+  }
+
+  void _itemNotifierListener() {
+    _textController.text = _formattedItem;
+  }
+
+  void _setValue() {
+    widget.onChanged(_itemNotifier.value);
+    if (_overlayController.isShowing) {
+      _overlayController.hide();
+    }
+    AppTableView.maybeOf(context)?.controller.reload();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _itemNotifier.addListener(_itemNotifierListener);
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    _itemNotifier.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return CompositedTransformTarget(
+      link: _link,
+      child: OverlayPortal(
+        controller: _overlayController,
+        overlayChildBuilder: (context) {
+          return CompositedTransformFollower(
+            link: _link,
+            showWhenUnlinked: false,
+            targetAnchor: Alignment.bottomLeft,
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 300,
+                constraints: const BoxConstraints(maxHeight: 500),
+                child: TapRegion(
+                  onTapOutside: (_) => _overlayController.hide(),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: widget.items.length,
+                        padding: EdgeInsets.zero,
+                        itemBuilder: (context, index) {
+                          final item = widget.items[index];
+
+                          return ListTile(
+                            visualDensity: VisualDensity.compact,
+                            title: Text(
+                              widget.asString?.call(item) ?? item.toString(),
+                            ),
+                            onTap: () {
+                              _itemNotifier.value = item;
+                              _setValue();
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+        child: ListenableBuilder(
+          listenable: _itemNotifier,
+          builder: (context, child) {
+            final hasItem = _itemNotifier.value != null;
+
+            return FilterChip(
+              label: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Flexible(
+                    child: Text(
+                      [
+                        widget.label,
+                        if (hasItem) _formattedItem,
+                      ].join(': '),
+                    ),
+                  ),
+                  if (!hasItem) ...[
+                    const Spacing(),
+                    const Icon(Icons.arrow_drop_down, size: 16),
+                  ],
+                ],
+              ),
+              selected: hasItem,
+              showCheckmark: false,
+              deleteIconColor: context.colorScheme.onPrimary,
+              onSelected: (_) {
+                _overlayController.show();
+              },
+              onDeleted: hasItem
+                  ? () {
+                      _itemNotifier.value = null;
+                      _setValue();
+                    }
+                  : null,
+            );
+          },
+        ),
+      ),
+    );
   }
 }
